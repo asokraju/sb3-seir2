d30231c4f4705b7376baa98ae7b8ad14544d1300
guanxy-think <55400478+guanxy-think@users.noreply.github.com>
2021-04-29 11:05:36 -0700

    test message

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         'N' : 5000, # number of samples to plot
         'theta':{0: 113.92, 1: 87.15, 2: 107.97},
         'w_all' : [0.0 , 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 ],
-        'sel_w' : [0.35],
+        'sel_w' : [0.4],  # the weight values to be used in the code
         'Senarios' : [ 'BaseLine', 'Senario_1', 'Senario_2'],
         'a_map' : {0:'LockDown', 1:'Social Distancing', 2:'Open'},
         'initial_state':{
@@ -44,9 +44,9 @@ if __name__ == '__main__':
             1:[99962.0, 7.0, 14.0, 17.0],
             2:[99905.0, 22.0, 39.0, 34.0]
             },
-        'seed': 2424,
+        'seed': 2424, #random number generator seed
         'policy_kwargs': dict(activation_fn=th.nn.ReLU,
-                     net_arch=[128, dict(pi=[512, 512], vf=[512, 512])])
+                     net_arch=[128, dict(pi=[512, 512], vf=[512, 512])])  #NN parameters
     }
     np.random.seed(args['seed'])
     states = random_states(args['N'])
