207b4245bddcad52c96468b1dd7bceab46830d36
guanxy-think <55400478+guanxy-think@users.noreply.github.com>
2021-05-05 15:10:13 -0700

    Weight of 0.2 for all scenarios completed

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         'N' : 5000, # number of samples to plot
         'theta':{0: 113.92, 1: 87.15, 2: 107.97},
         'w_all' : [0.0 , 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 ],
-        'sel_w' : [0.4],  # the weight values to be used in the code
+        'sel_w' : [0.2, 0.1],  # the weight values to be used in the code
         'Senarios' : [ 'BaseLine', 'Senario_1', 'Senario_2'],
         'a_map' : {0:'LockDown', 1:'Social Distancing', 2:'Open'},
         'initial_state':{
