5062616e1be6d45abef47a31c1cfee718c11fb34
guanxy-think <55400478+guanxy-think@users.noreply.github.com>
2021-05-05 22:32:34 -0700

    Weigh of 0.1 for all scenarios completed

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         'N' : 5000, # number of samples to plot
         'theta':{0: 113.92, 1: 87.15, 2: 107.97},
         'w_all' : [0.0 , 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 ],
-        'sel_w' : [0.2, 0.1],  # the weight values to be used in the code
+        'sel_w' : [0.1],  # the weight values to be used in the code
         'Senarios' : [ 'BaseLine', 'Senario_1', 'Senario_2'],
         'a_map' : {0:'LockDown', 1:'Social Distancing', 2:'Open'},
         'initial_state':{
@@ -79,10 +79,10 @@ if __name__ == '__main__':
         baseline_model_dir = dir_w + args['Senarios'][0] + "/"
 
         Scenario_1_model_dir = dir_w + args['Senarios'][1] + "/"
-        C, DF1 = CM(states, Scenario_actions[0], Scenario_actions[1], save_fig=True, fig_name=Scenario_1_model_dir+'confusion.jpg')
+        C, DF1 = CM(states, Scenario_actions[0], Scenario_actions[1], save_fig=True, fig_name=Scenario_1_model_dir+'confusion.pdf')
         C.to_csv(Scenario_1_model_dir+'C.csv')
         Scenario_2_model_dir = dir_w + args['Senarios'][2] + "/"
-        C, DF2 = CM(states, Scenario_actions[0], Scenario_actions[2], save_fig=True, fig_name=Scenario_2_model_dir+'confusion.jpg')
+        C, DF2 = CM(states, Scenario_actions[0], Scenario_actions[2], save_fig=True, fig_name=Scenario_2_model_dir+'confusion.pdf')
         C.to_csv(Scenario_2_model_dir+'C.csv')
 
         df = pd.DataFrame(states, columns=['Susceptible', 'Exposed', 'Infected', 'Recovered'])
