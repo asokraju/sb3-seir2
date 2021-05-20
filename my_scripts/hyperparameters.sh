3c912940fa1fd472a38da7238ba87fcb730c4a72
guanxy-think <55400478+guanxy-think@users.noreply.github.com>
2021-05-20 08:36:58 -0700

    results_Yang

--- my_scripts/hyperparameters.sh
+++ my_scripts/hyperparameters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-for test_name in test_1
+for test_name in test_yang_1
 do
   for env_id in gym_seir:seir-b-v0
   do
@@ -8,17 +8,17 @@ do
     do
       for Senario in 0
       do
-        for health_cost_scale in 600.0
+        for health_cost_scale in 581.0
         do
-          for rho_per_week in 0.02 0.01 0.03
+          for rho_per_week in 0.00 0.01 0.02 0.04 0.06 0.08 0.1
           do
-            for hospital_beds_ratio in 0.005 0.001 0.01
+            for hospital_beds_ratio in 0.00287 1.0
             do
-              for max_hospital_cost in 100.0 200.0
+              for max_hospital_cost in 100.0
               do
                 for seed in 2345
                 do
-                  C:/Users/kkris/Documents/GitHub/sb3-seir2/my_scripts/batch.sh $test_name $env_id $weight $Senario $health_cost_scale $rho_per_week $hospital_beds_ratio $max_hospital_cost $seed
+                  C:/Users/guanxy/Documents/GitHub/sb3-seir2/my_scripts/batch.sh $test_name $env_id $weight $Senario $health_cost_scale $rho_per_week $hospital_beds_ratio $max_hospital_cost $seed
                 done
               done
             done
